7da0441ed1e92b52b0e3ec03b45bb538e652fc94
moafaq <imoafaqsimbawa@gmail.com>
2020-08-15 18:47:13 +0300

    minor changes

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-database_path = '#'
+database_path = ''
 db = SQLAlchemy()
 
 def create_app():
