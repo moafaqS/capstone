4e7e4b670cd6e124bbb9d36880b7b99a85900f8f
moafaq <imoafaqsimbawa@gmail.com>
2020-08-17 15:29:55 +0300

    minor

--- app.py
+++ app.py
@@ -216,6 +216,8 @@ def create_app():
           "message": error.error['description']
       }), error.status_code
 
+  if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
   return app
 
 
